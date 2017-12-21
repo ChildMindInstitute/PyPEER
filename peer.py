526122b66ebcb3a7198e5bd5305f30252aaeedad
Jake Son <jake.son@childmind.org>
2017-12-21 11:49:02 -0500

    SVR model correctly predicts values when tested on training set -- next step is to import additional PEER scan data and test on testing set.

--- peer.py
+++ peer.py
@@ -21,6 +21,7 @@ monitor_height = 1050
 # Import data
 img = nib.load('peer2_processed.nii.gz')
 data = img.get_data()
+testing = nib.load('peer1_processed.nii.gz')
 print(img.shape)
 
 # Vectorize data into single np array
@@ -38,38 +39,38 @@ df = np.asarray(listed)
 # Create np array that contains all fixation locations, separated by x and y coordinates
 
 fixations = pd.read_csv('stim_vals.csv')
-x_targets = np.repeat(np.array(fixations['pos_x']), 5)*monitor_width
-y_targets = np.repeat(np.array(fixations['pos_y']), 5)*monitor_height
+x_targets = np.repeat(np.array(fixations['pos_x']), 5)*monitor_width/2
+y_targets = np.repeat(np.array(fixations['pos_y']), 5)*monitor_height/2
 
-# For visualization
-for num in [0, 50]:
-    visual = image.index_img(img, num)
-
-    slice_data = visual.get_data()[:, :, 15:30]
-
-    plotting.plot_stat_map(visual)
-    plotting.show()
+# # For visualization
+# for num in [0, 50]:
+#     visual = image.index_img(img, num)
+#     slice_data = visual.get_data()[:, :, 15:30]
+#     plotting.plot_stat_map(visual)
+#     plotting.show()
 
 # Train SVR classifier on first two calibrations and predict values for third calibration
 train_vectors = 'Contains vectors of voxel intensity values for each axial slice'
 test_vectors = 'Contains vectors of voxel intensity values for each axial slice'
-train_x = 'Contains x coordinates of first two calibration fixations'
-train_y = 'Contains y coordinates of first two calibration fixations'
 test_vectors = 'Contains x coordinates of third calibration fixations'
 test_vectors = 'Contains x coordinates of third calibration fixations'
 
-# Testing
+# Testing - Remove after validation
 train_vectors = df
 train_x = x_targets
 
-classifier = SVR(kernel='poly', degree=10, C=100, epsilon=.001)
-predicted_x = classifier.fit(train_vectors, train_x).predict(test_vectors)
-predicted_y = classifier.fit(train_vectors, train_y).predict(test_vectors)
+classifier = SVR(kernel='rbf', degree=25, C=1000, epsilon=.001)
+# Validation
+validation_x = classifier.fit(train_vectors, x_targets).predict(train_vectors)
+validation_y = classifier.fit(train_vectors, y_targets).predict(train_vectors)
+# Testing
+predicted_x = classifier.fit(train_vectors, x_targets).predict(test_vectors)
+predicted_y = classifier.fit(train_vectors, y_targets).predict(test_vectors)
 
 # Plot SVR predictions against targets
 plt.figure()
 plt.plot(predicted_x, predicted_y, color='b')
-plt.plot(train_x, train_y, color='r.')
+plt.plot(x_targets, y_targets, color='r.')
 plt.xlabel('x-position')
 plt.ylabel('y-position')
 plt.title('Support Vector Regression - PEER')
