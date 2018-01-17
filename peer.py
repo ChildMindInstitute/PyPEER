fb75c7b174a45b522b49c343275e06374b6eb3a9
Jake Son <jake.son@childmind.org>
2018-01-17 18:35:06 -0500

    Script runs through all participants in given data path and returns output figures to specified output path. Next step will be to include error measures to assess for correlation between RMS and meanFD. Subject parameters are also automatically updated in new subj_params.csv, with voxel (x,y,z) features set arbitrarily for now until all PEER scans are registered to the same template.

--- peer.py
+++ peer.py
@@ -3,9 +3,11 @@
 
 # Screen size: 1680 x 1050 - from follow_the_dot_lastrun.py in ~/Desktop/HBN_Peer
 
-data_path = '/home/json/Desktop/PEER_bash/sub-5072755/'
+data_path = '/home/json/Desktop/PEER_bash/'
+output_path = '/home/json/Desktop/peer/Figures'
 
 import os
+import csv
 import numpy as np
 import pandas as pd
 import nibabel as nib
@@ -19,137 +21,163 @@ monitor_width = 1680
 monitor_height = 1050
 
 # #############################################################################
+# Update subject parameters sheet with new participants
+
+# params = pd.read_csv('subj_params.csv', index_col='Subject', dtype=object)
+# subj_list = params.index.values.tolist()
+#
+# x_b = 12
+# x_e = 40
+# y_b = 35
+# y_e = 50
+# z_b = 2
+# z_e = 13
+#
+# with open('subj_params.csv', 'a') as updated_params:
+#     writer = csv.writer(updated_params)
+#
+#     for subject in os.listdir(data_path):
+#         if any(subject in x for x in subj_list) and 'txt' not in subject:
+#             print(subject + ' is already in subj_params.csv')
+#         elif 'txt' not in subject:
+#             writer.writerow([subject, x_b, x_e, y_b, y_e, z_b, z_e])
+#             print('New participant ' + subject + ' was added')
+
+# #############################################################################
 # Import data
 
-set = 'n5072755'
+params = pd.read_csv('subj_params.csv', index_col='Subject', dtype=object)
+subj_list = params.index.values.tolist()
 
-params = pd.read_excel('subj_params.xlsx', index_col='Subject', dtype=object)
-x_begin_slice = params.loc[set, 'x_start']
-x_end_slice = params.loc[set, 'x_end']
-y_begin_slice = params.loc[set, 'y_start']
-y_end_slice = params.loc[set, 'y_end']
-z_begin_slice = params.loc[set, 'z_start']
-z_end_slice = params.loc[set, 'z_end']
+for set in subj_list:
 
-training1 = nib.load(data_path + 'PEER1_resampled.nii.gz')
-training2 = nib.load(data_path + 'PEER3_resampled.nii.gz')
-testing = nib.load(data_path + 'PEER2_resampled.nii.gz')
-training1_data = training1.get_data()
-training2_data = training2.get_data()
-testing_data = testing.get_data()
+    x_begin_slice = int(params.loc[set, 'x_start'])
+    x_end_slice = int(params.loc[set, 'x_end'])
+    y_begin_slice = int(params.loc[set, 'y_start'])
+    y_end_slice = int(params.loc[set, 'y_end'])
+    z_begin_slice = int(params.loc[set, 'z_start'])
+    z_end_slice = int(params.loc[set, 'z_end'])
 
-# #############################################################################
-# Vectorize data into single np array
+    print('Beginning analysis on participant ' + set)
 
-listed1 = []
-listed2 = []
-listed_testing = []
+    training1 = nib.load(data_path + set + '/PEER1_resampled.nii.gz')
+    training2 = nib.load(data_path + set + '/PEER3_resampled.nii.gz')
+    testing = nib.load(data_path + set + '/PEER2_resampled.nii.gz')
+    training1_data = training1.get_data()
+    training2_data = training2.get_data()
+    testing_data = testing.get_data()
 
-for tr in range(int(training1_data.shape[3])):
+    # #############################################################################
+    # Vectorize data into single np array
 
+    listed1 = []
+    listed2 = []
+    listed_testing = []
 
-    tr_data1 = training1_data[x_begin_slice:x_end_slice, y_begin_slice:y_end_slice, z_begin_slice:z_end_slice, tr]
-    tr_data2 = training2_data[x_begin_slice:x_end_slice, y_begin_slice:y_end_slice, z_begin_slice:z_end_slice, tr]
-    te_data = testing_data[x_begin_slice:x_end_slice, y_begin_slice:y_end_slice, z_begin_slice:z_end_slice, tr]
-    vectorized1 = np.array(tr_data1.ravel())
-    vectorized2 = np.array(tr_data2.ravel())
-    vectorized_testing = np.array(te_data.ravel())
-    listed1.append(vectorized1)
-    listed2.append(vectorized2)
-    listed_testing.append(vectorized_testing)
+    for tr in range(int(training1_data.shape[3])):
 
-train_vectors1 = np.asarray(listed1)
-train_vectors2 = np.asarray(listed2)
-test_vectors = np.asarray(listed_testing)
+        tr_data1 = training1_data[x_begin_slice:x_end_slice, y_begin_slice:y_end_slice, z_begin_slice:z_end_slice, tr]
+        tr_data2 = training2_data[x_begin_slice:x_end_slice, y_begin_slice:y_end_slice, z_begin_slice:z_end_slice, tr]
+        te_data = testing_data[x_begin_slice:x_end_slice, y_begin_slice:y_end_slice, z_begin_slice:z_end_slice, tr]
+        vectorized1 = np.array(tr_data1.ravel())
+        vectorized2 = np.array(tr_data2.ravel())
+        vectorized_testing = np.array(te_data.ravel())
+        listed1.append(vectorized1)
+        listed2.append(vectorized2)
+        listed_testing.append(vectorized_testing)
 
-# #############################################################################
-# Averaging training signal
+    train_vectors1 = np.asarray(listed1)
+    train_vectors2 = np.asarray(listed2)
+    test_vectors = np.asarray(listed_testing)
 
-averaged_train1 = []
-averaged_train2 = []
-averaged_test = []
-for num in [i*5 for i in range(27)]:
+    # #############################################################################
+    # Averaging training signal
 
-    averaged_train1.append(np.average(train_vectors1[num:num+5], axis=0))
-    averaged_train2.append(np.average(train_vectors2[num:num+5], axis=0))
-    # averaged_test.append(np.average(test_vectors[num:num+5], axis=0))
+    averaged_train1 = []
+    averaged_train2 = []
+    averaged_test = []
+    for num in [i*5 for i in range(27)]:
 
-if averaged_train2 == []:
-    train_vectors = np.asarray(averaged_train1)
-    print('just one')
-else:
-    train_vectors = np.asarray(averaged_train1 + averaged_train2)
-    print('concatenated')
+        averaged_train1.append(np.average(train_vectors1[num:num+5], axis=0))
+        averaged_train2.append(np.average(train_vectors2[num:num+5], axis=0))
+        # averaged_test.append(np.average(test_vectors[num:num+5], axis=0))
 
-# test_vectors = np.asarray(averaged_test)
-
-# #############################################################################
-# Import coordinates for fixations
-
-fixations = pd.read_csv('stim_vals.csv')
-# x_targets = np.repeat(np.array(fixations['pos_x']), 1)*monitor_width/2
-# y_targets = np.repeat(np.array(fixations['pos_y']), 1)*monitor_height/2
-x_targets = np.tile(np.repeat(np.array(fixations['pos_x']), 1)*monitor_width/2, 2)
-y_targets = np.tile(np.repeat(np.array(fixations['pos_y']), 1)*monitor_height/2, 2)
-
-# #############################################################################
-# Create SVR Model
-
-# # Manual Model
-# clfx = SVR(kernel='linear', C=100, epsilon=.001)
-# clfx.fit(train_vectors, x_targets)
-# clfy = SVR(kernel='linear', C=100, epsilon=.001)
-# clfy.fit(train_vectors, y_targets)
-# predicted_x = clfx.predict(test_vectors)
-# predicted_y = clfy.predict(test_vectors)
-
-# GridSearch Model
-GS_model = SVR(kernel='linear')
-parameters = {'kernel': ('linear', 'poly'), 'C': [100, 1000, 2500, 5000, 10000],
-              'epsilon': [.01, .005, .001]}
-clfx = GridSearchCV(GS_model, parameters)
-clfx.fit(train_vectors, x_targets)
-clfy = GridSearchCV(GS_model, parameters)
-clfy.fit(train_vectors, y_targets)
-predicted_x = clfx.predict(test_vectors)
-predicted_y = clfy.predict(test_vectors)
-print(predicted_x)
-print(predicted_y)
-
-# #############################################################################
-# Plot SVR predictions against targets
-
-plt.figure(figsize=(10, 10))
-subplot_i = 0
-
-for num in range(27):
-
-    nums = num * 5
-
-    if num not in [18, 25]:
-
-        subplot_i += 1
-        plt.subplot(5, 5, subplot_i)
-        axes = plt.gca()
-        axes.set_xlim([-1500, 1500])
-        axes.set_ylim([-1200, 1200])
-        plt.scatter(x_targets[num], y_targets[num], color='k', marker='x')
-        plt.scatter(predicted_x[nums:nums+5], predicted_y[nums:nums+5], s=5)
-
-    elif num == 18 or num == 25:
-
-        plt.subplot(5, 5, 1)
-        axes = plt.gca()
-        axes.set_xlim([-1500, 1500])
-        axes.set_ylim([-1200, 1200])
-        plt.scatter(x_targets[num], y_targets[num], color='k', marker='x')
-        plt.scatter(predicted_x[nums:nums+5], predicted_y[nums:nums+5], s=5)
-
+    if averaged_train2 == []:
+        train_vectors = np.asarray(averaged_train1)
+        print('Has two valid PEER scans')
     else:
-        continue
+        train_vectors = np.asarray(averaged_train1 + averaged_train2)
+        print('Has three valid PEER scans')
 
-# plt.savefig(os.path.join('/Users/jake.son/PycharmProjects/PEER/Figures', set + '.png'), bbox_inches='tight', dpi=600)
-plt.show()
+    # test_vectors = np.asarray(averaged_test)
+
+    # #############################################################################
+    # Import coordinates for fixations
+
+    fixations = pd.read_csv('stim_vals.csv')
+    # x_targets = np.repeat(np.array(fixations['pos_x']), 1)*monitor_width/2
+    # y_targets = np.repeat(np.array(fixations['pos_y']), 1)*monitor_height/2
+    x_targets = np.tile(np.repeat(np.array(fixations['pos_x']), 1)*monitor_width/2, 2)
+    y_targets = np.tile(np.repeat(np.array(fixations['pos_y']), 1)*monitor_height/2, 2)
+
+    # #############################################################################
+    # Create SVR Model
+
+    # # Manual Model
+    # clfx = SVR(kernel='linear', C=100, epsilon=.001)
+    # clfx.fit(train_vectors, x_targets)
+    # clfy = SVR(kernel='linear', C=100, epsilon=.001)
+    # clfy.fit(train_vectors, y_targets)
+    # predicted_x = clfx.predict(test_vectors)
+    # predicted_y = clfy.predict(test_vectors)
+
+    # GridSearch Model
+    GS_model = SVR(kernel='linear')
+    parameters = {'kernel': ('linear', 'poly'), 'C': [100, 1000, 2500, 5000, 10000],
+                  'epsilon': [.01, .005, .001]}
+    clfx = GridSearchCV(GS_model, parameters)
+    clfx.fit(train_vectors, x_targets)
+    clfy = GridSearchCV(GS_model, parameters)
+    clfy.fit(train_vectors, y_targets)
+    predicted_x = clfx.predict(test_vectors)
+    predicted_y = clfy.predict(test_vectors)
+
+    # #############################################################################
+    # Plot SVR predictions against targets
+
+    plt.figure(figsize=(10, 10))
+    subplot_i = 0
+
+    for num in range(27):
+
+        nums = num * 5
+
+        if num not in [18, 25]:
+
+            subplot_i += 1
+            plt.subplot(5, 5, subplot_i)
+            axes = plt.gca()
+            axes.set_xlim([-1500, 1500])
+            axes.set_ylim([-1200, 1200])
+            plt.scatter(x_targets[num], y_targets[num], color='k', marker='x')
+            plt.scatter(predicted_x[nums:nums+5], predicted_y[nums:nums+5], s=5)
+
+        elif num == 18 or num == 25:
+
+            plt.subplot(5, 5, 1)
+            axes = plt.gca()
+            axes.set_xlim([-1500, 1500])
+            axes.set_ylim([-1200, 1200])
+            plt.scatter(x_targets[num], y_targets[num], color='k', marker='x')
+            plt.scatter(predicted_x[nums:nums+5], predicted_y[nums:nums+5], s=5)
+
+        else:
+            continue
+
+    plt.savefig(os.path.join(output_path, set + '.png'), bbox_inches='tight', dpi=600)
+    # plt.show()
+
+    print('Completed participant ' + set)
 
 # #############################################################################
 # Plot SVM weights as heatmap (one participant at a time since number of features is variable)
