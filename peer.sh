b424964f9a7de0d22dff2a3fa76310d25e7f6261
Jake Son <jake.son@childmind.org>
2018-01-24 14:42:07 -0500

    Automates analysis for all participants in Rutgers dataset, with 2 or 3 valid peer scans while excluding those without sufficient training data.

--- peer.sh
+++ peer.sh
@@ -1,6 +1,6 @@
 # This script runs PEER on the participants of a given directory, assuming that they each have 3 PEER scans.
 
-data='/data2/HBNcore/CMI_HBN_Data/MRI/RU/data_Backup'
+data='/data2/HBNcore/CMI_HBN_Data/MRI/RU/data'
 outpath='/home/json/Desktop/PEER_bash'
 
 rm $outpath/command_list.txt
@@ -11,7 +11,11 @@ cd $data
 
 for sub in $(ls);do
 
-	if grep "$sub" "/home/json/Desktop/PEER_data/testing_on_new.txt"; then	
+#	if grep "$sub" "/home/json/Desktop/PEER_data/testing_on_new.txt"; then	
+
+	n=`find "$data/$sub/func" -iname '*peer*' -iname '*nii.gz*' |wc -l`
+
+	if [ $n -gt "1" ]; then
 
 		num=0
 
@@ -20,18 +24,15 @@ for sub in $(ls);do
 			if [[ $nifti_file == *"peer_run-1"* ]];then
 				echo -n "mcflirt -in $nifti_file -out $outpath/$sub/peer1_mcf.nii.gz" >> $outpath/command_list.txt
 				echo ";fslroi $outpath/$sub/peer1_mcf.nii.gz $outpath/$sub/template_1.nii.gz 67 1" >> $outpath/command_list.txt
-				num=$(($num + 1))
 				
 
 			elif [[ $nifti_file == *"peer_run-2"* ]];then
 				echo -n "mcflirt -in $nifti_file -out $outpath/$sub/peer2_mcf.nii.gz" >> $outpath/command_list.txt
 				echo ";fslroi $outpath/$sub/peer2_mcf.nii.gz $outpath/$sub/template_2.nii.gz 67 1" >> $outpath/command_list.txt
-				num=$(($num + 1))
 
 			elif [[ $nifti_file == *"peer_run-3"* ]];then
 				echo -n "mcflirt -in $nifti_file -out $outpath/$sub/peer3_mcf.nii.gz" >> $outpath/command_list.txt
 				echo ";fslroi $outpath/$sub/peer3_mcf.nii.gz $outpath/$sub/template_3.nii.gz 67 1" >> $outpath/command_list.txt
-				num=$(($num + 1))
 
 			fi
 
@@ -39,13 +40,13 @@ for sub in $(ls);do
 
 		mkdir $outpath'/'$sub
 
-	if [ "$num" -eq "2" ]; then
+	if [ "$n" -eq "2" ]; then
 		
 		echo -n "mri_robust_template --mov $outpath/$sub/template_1.nii.gz $outpath/$sub/template_2.nii.gz --template $outpath/$sub/mean.nii.gz --satit" >> $outpath/mri_template.txt
 
 		echo ";echo $sub registration template complete" >> $outpath/mri_template.txt
 
-	elif [ "$num" -eq "3" ]; then
+	elif [ "$n" -eq "3" ]; then
 
 		echo -n "mri_robust_template --mov $outpath/$sub/template_1.nii.gz $outpath/$sub/template_2.nii.gz $outpath/$sub/template_3.nii.gz --template $outpath/$sub/mean.nii.gz --satit" >> $outpath/mri_template.txt
 
@@ -54,6 +55,8 @@ for sub in $(ls);do
 	fi
 
 	fi
+
+#	fi
 
 done
 
