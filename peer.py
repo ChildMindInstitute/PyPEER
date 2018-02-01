a2ae8fbfeeb0fb1c93c02a8bd0c11567711c17f0
Jake Son <jake.son@childmind.org>
2018-02-01 13:16:50 -0500

    Restructured code to include more funcitons for modularity with different experimental designs (e.g. 1 vs 2 peer scans for training). close #19

--- peer.py
+++ peer.py
@@ -33,11 +33,6 @@ def gs_regress(data, xb, xe, yb, ye, zb, ze):
 
     X = X[:, 1:]
 
-    # csv_filename = csv_filename[1:]
-    # csv_filename += '.csv'
-    # csv_filename = os.path.join(os.getcwd(), csv_filename)
-    # np.savetxt(csv_filename, X, delimiter='\t')
-
     Y = data[global_mask].T
     B = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(Y)
 
@@ -47,8 +42,107 @@ def gs_regress(data, xb, xe, yb, ye, zb, ze):
 
     return data
 
+
+def remove_out(group):
+
+    temp = params[group][:500].tolist()
+    temp = [float(x) for x in temp]
+    temp_mean = np.array(temp).mean()
+    temp_stdv = np.array(temp).std()
+    cap = temp_mean + 3*temp_stdv
+
+    modified_group = [x for x in temp if 0 < x < cap]
+
+    return modified_group
+
+
+def create_model(train_vectors, test_vectors, x_targets, y_targets):
+
+    print('making model')
+
+    # GridSearch Model
+    GS_model = SVR(kernel='linear')
+    parameters = {'kernel': ('linear', 'poly'), 'C': [100, 1000, 2500, 5000, 10000],
+                  'epsilon': [.01, .005, .001]}
+    clfx = GridSearchCV(GS_model, parameters)
+    clfx.fit(train_vectors, x_targets)
+    clfy = GridSearchCV(GS_model, parameters)
+    clfy.fit(train_vectors, y_targets)
+    predicted_x = clfx.predict(test_vectors)
+    predicted_y = clfy.predict(test_vectors)
+
+    print('model created')
+
+    return predicted_x, predicted_y
+
+
+def scatter_plot(x_targets, y_targets, predicted_x, predicted_y, plot=False, save=False, show=False):
+
+    if plot:
+
+        plt.figure(figsize=(10, 10))
+        subplot_i = 0
+
+        for num in range(27):
+
+            nums = num * 5
+
+            if num not in [18, 25]:
+
+                subplot_i += 1
+                plt.subplot(5, 5, subplot_i)
+                axes = plt.gca()
+                axes.set_xlim([-1500, 1500])
+                axes.set_ylim([-1200, 1200])
+                plt.scatter(x_targets[num], y_targets[num], color='k', marker='x')
+                plt.scatter(predicted_x[nums:nums + 5], predicted_y[nums:nums + 5], s=5)
+
+            elif num == 18 or num == 25:
+
+                plt.subplot(5, 5, 1)
+                axes = plt.gca()
+                axes.set_xlim([-1500, 1500])
+                axes.set_ylim([-1200, 1200])
+                plt.scatter(x_targets[num], y_targets[num], color='k', marker='x')
+                plt.scatter(predicted_x[nums:nums + 5], predicted_y[nums:nums + 5], s=5)
+
+            else:
+                continue
+
+        if save:
+
+            plt.savefig(os.path.join(output_path, name + '.png'), bbox_inches='tight', dpi=600)
+
+        if show:
+
+            plt.show()
+            print('show')
+
+
+def data_processing():
+
+    averaged_train1 = []
+    averaged_train2 = []
+
+    for num in [i*5 for i in range(27)]:
+
+        averaged_train1.append(np.average(train_vectors1[num:num+5], axis=0))
+
+        if scan_count == 3:
+            averaged_train2.append(np.average(train_vectors2[num:num+5], axis=0))
+
+    if scan_count == 2:
+        train_vectors = np.asarray(averaged_train1)
+        print('Has ' + str(scan_count) + ' valid PEER scans')
+    elif scan_count == 3:
+        train_vectors = np.asarray(averaged_train1 + averaged_train2)
+        print('Has ' + str(scan_count) + ' valid PEER scans')
+
+    return train_vectors
+
 data_path = '/data2/Projects/Jake/RU/'
-qap_path = '/data2/HBNcore/CMI_HBN_Data/MRI/RU/QAP/qap_functional_temporal.csv'
+qap_path_RU = '/data2/HBNcore/CMI_HBN_Data/MRI/RU/QAP/qap_functional_temporal.csv'
+qap_path_CBIC = '/data2/HBNcore/CMI_HBN_Data/MRI/CBIC/QAP/qap_functional_temporal.csv'
 output_path = '/home/json/Desktop/peer/Figures'
 
 monitor_width = 1680
@@ -60,9 +154,8 @@ monitor_height = 1050
 params = pd.read_csv('subj_params.csv', index_col='subject', dtype=object)
 sub_ref = params.index.values.tolist()
 
-qap = pd.read_csv(qap_path, dtype=object)
+qap = pd.read_csv(qap_path_RU, dtype=object)
 qap['Participant'] = qap['Participant'].str.replace('_', '-')
-# qap = qap.set_index('Participant')
 
 x_b = 12; x_e = 40; y_b = 35; y_e = 50; z_b = 2; z_e = 13
 
@@ -84,29 +177,29 @@ with open('subj_params.csv', 'a') as updated_params:
 
 params = pd.read_csv('subj_params.csv', index_col='subject', dtype=object)
 
-ru_set = params.index.values.tolist()[:350]
+full_set = params.index.values.tolist()[350:]
 
-for set in ['sub-5773707']:
+for name in ['sub-5378545']:
 
-    xb = int(params.loc[set, 'x_start'])
-    xe = int(params.loc[set, 'x_end'])
-    yb = int(params.loc[set, 'y_start'])
-    ye = int(params.loc[set, 'y_end'])
-    zb = int(params.loc[set, 'z_start'])
-    ze = int(params.loc[set, 'z_end'])
+    xb = int(params.loc[name, 'x_start'])
+    xe = int(params.loc[name, 'x_end'])
+    yb = int(params.loc[name, 'y_start'])
+    ye = int(params.loc[name, 'y_end'])
+    zb = int(params.loc[name, 'z_start'])
+    ze = int(params.loc[name, 'z_end'])
 
     try:
 
-        print('Beginning analysis on participant ' + set)
+        print('Beginning analysis on participant ' + name)
 
-        training1 = nib.load(data_path + set + '/PEER1_resampled.nii.gz')
+        training1 = nib.load(data_path + name + '/PEER1_resampled.nii.gz')
         training1_data = training1.get_data()
-        testing = nib.load(data_path + set + '/PEER2_resampled.nii.gz')
+        testing = nib.load(data_path + name + '/PEER2_resampled.nii.gz')
         testing_data = testing.get_data()
 
         try:
 
-            training2 = nib.load(data_path + set + '/PEER3_resampled.nii.gz')
+            training2 = nib.load(data_path + name + '/PEER3_resampled.nii.gz')
             training2_data = training2.get_data()
             scan_count = 3
 
@@ -117,16 +210,16 @@ for set in ['sub-5773707']:
         # #############################################################################
         # Global Signal Regression
 
-        if scan_count == 2:
-
-            training1_data = gs_regress(training1_data, xb, xe, yb, ye, zb, ze)
-            testing_data = gs_regress(testing_data, xb, xe, yb, ye, zb, ze)
-
-        elif scan_count == 3:
-
-            training1_data = gs_regress(training1_data, xb, xe, yb, ye, zb, ze)
-            training2_data = gs_regress(training2_data, xb, xe, yb, ye, zb, ze)
-            testing_data = gs_regress(testing_data, xb, xe, yb, ye, zb, ze)
+        # if scan_count == 2:
+        #
+        #     training1_data = gs_regress(training1_data, xb, xe, yb, ye, zb, ze)
+        #     testing_data = gs_regress(testing_data, xb, xe, yb, ye, zb, ze)
+        #
+        # elif scan_count == 3:
+        #
+        #     training1_data = gs_regress(training1_data, xb, xe, yb, ye, zb, ze)
+        #     training2_data = gs_regress(training2_data, xb, xe, yb, ye, zb, ze)
+        #     testing_data = gs_regress(testing_data, xb, xe, yb, ye, zb, ze)
 
         # #############################################################################
         # Vectorize data into single np array
@@ -160,23 +253,9 @@ for set in ['sub-5773707']:
         # #############################################################################
         # Averaging training signal
 
-        averaged_train1 = []
-        averaged_train2 = []
-        averaged_test = []
 
-        for num in [i*5 for i in range(27)]:
 
-            averaged_train1.append(np.average(train_vectors1[num:num+5], axis=0))
-
-            if scan_count == 3:
-                averaged_train2.append(np.average(train_vectors2[num:num+5], axis=0))
-
-        if scan_count == 2:
-            train_vectors = np.asarray(averaged_train1)
-            print('Has ' + str(scan_count) + ' valid PEER scans')
-        elif scan_count == 3:
-            train_vectors = np.asarray(averaged_train1 + averaged_train2)
-            print('Has ' + str(scan_count) + ' valid PEER scans')
+        train_vectors = data_processing()
 
         # #############################################################################
         # Import coordinates for fixations
@@ -188,57 +267,14 @@ for set in ['sub-5773707']:
         # #############################################################################
         # Create SVR Model
 
-        print('making model')
-
-        # GridSearch Model
-        GS_model = SVR(kernel='linear')
-        parameters = {'kernel': ('linear', 'poly'), 'C': [100, 1000, 2500, 5000, 10000],
-                      'epsilon': [.01, .005, .001]}
-        clfx = GridSearchCV(GS_model, parameters)
-        clfx.fit(train_vectors, x_targets)
-        clfy = GridSearchCV(GS_model, parameters)
-        clfy.fit(train_vectors, y_targets)
-        predicted_x = clfx.predict(test_vectors)
-        predicted_y = clfy.predict(test_vectors)
-
-        print('model created')
+        predicted_x, predicted_y = create_model(train_vectors, test_vectors, x_targets, y_targets)
 
         # #############################################################################
         # Plot SVR predictions against targets
 
-        plt.figure(figsize=(10, 10))
-        subplot_i = 0
+        scatter_plot(x_targets, y_targets, predicted_x, predicted_y, plot=True, save=False, show=True)
 
-        for num in range(27):
-
-            nums = num * 5
-
-            if num not in [18, 25]:
-
-                subplot_i += 1
-                plt.subplot(5, 5, subplot_i)
-                axes = plt.gca()
-                axes.set_xlim([-1500, 1500])
-                axes.set_ylim([-1200, 1200])
-                plt.scatter(x_targets[num], y_targets[num], color='k', marker='x')
-                plt.scatter(predicted_x[nums:nums+5], predicted_y[nums:nums+5], s=5)
-
-            elif num == 18 or num == 25:
-
-                plt.subplot(5, 5, 1)
-                axes = plt.gca()
-                axes.set_xlim([-1500, 1500])
-                axes.set_ylim([-1200, 1200])
-                plt.scatter(x_targets[num], y_targets[num], color='k', marker='x')
-                plt.scatter(predicted_x[nums:nums+5], predicted_y[nums:nums+5], s=5)
-
-            else:
-                continue
-
-        # plt.savefig(os.path.join(output_path, set + '.png'), bbox_inches='tight', dpi=600)
-        # plt.show()
-
-        print('Completed participant ' + set)
+        print('Completed participant ' + name)
 
         # ###############################################################################
         # Get error measurements
@@ -260,26 +296,26 @@ for set in ['sub-5773707']:
         x_error = np.sqrt(np.sum(np.array(x_res))/135)
         y_error = np.sqrt(np.sum(np.array(y_res))/135)
 
-        params.loc[set, 'x_error_gsr'] = x_error
-        params.loc[set, 'y_error_gsr'] = y_error
-        params.loc[set, 'scan_count'] = scan_count
+        params.loc[name, 'x_error'] = x_error
+        params.loc[name, 'y_error'] = y_error
+        params.loc[name, 'scan_count'] = scan_count
         params.to_csv('subj_params.csv')
 
         if scan_count == 3:
 
-            try: # This is included in case the QAP csv doesn't already have the subject processed
+            try:
 
-                fd1 = float(qap[qap['Participant'] == set][qap['Series'] == 'func_peer_run_1']['RMSD (Mean)'])
-                fd2 = float(qap[qap['Participant'] == set][qap['Series'] == 'func_peer_run_2']['RMSD (Mean)'])
-                fd3 = float(qap[qap['Participant'] == set][qap['Series'] == 'func_peer_run_3']['RMSD (Mean)'])
-                dv1 = float(qap[qap['Participant'] == set][qap['Series'] == 'func_peer_run_1']['Std. DVARS (Mean)'])
-                dv2 = float(qap[qap['Participant'] == set][qap['Series'] == 'func_peer_run_2']['Std. DVARS (Mean)'])
-                dv3 = float(qap[qap['Participant'] == set][qap['Series'] == 'func_peer_run_3']['Std. DVARS (Mean)'])
+                fd1 = float(qap[qap['Participant'] == name][qap['Series'] == 'func_peer_run_1']['RMSD (Mean)'])
+                fd2 = float(qap[qap['Participant'] == name][qap['Series'] == 'func_peer_run_2']['RMSD (Mean)'])
+                fd3 = float(qap[qap['Participant'] == name][qap['Series'] == 'func_peer_run_3']['RMSD (Mean)'])
+                dv1 = float(qap[qap['Participant'] == name][qap['Series'] == 'func_peer_run_1']['Std. DVARS (Mean)'])
+                dv2 = float(qap[qap['Participant'] == name][qap['Series'] == 'func_peer_run_2']['Std. DVARS (Mean)'])
+                dv3 = float(qap[qap['Participant'] == name][qap['Series'] == 'func_peer_run_3']['Std. DVARS (Mean)'])
 
                 fd_score = np.average([fd1, fd2, fd3])
                 dvars_score = np.average([dv1, dv2, dv3])
-                params.loc[set, 'mean_fd'] = fd_score
-                params.loc[set, 'dvars'] = dvars_score
+                params.loc[name, 'mean_fd'] = fd_score
+                params.loc[name, 'dvars'] = dvars_score
 
             except:
 
@@ -289,15 +325,15 @@ for set in ['sub-5773707']:
 
             try:
 
-                fd1 = float(qap[qap['Participant'] == set][qap['Series'] == 'func_peer_run_1']['RMSD (Mean)'])
-                fd2 = float(qap[qap['Participant'] == set][qap['Series'] == 'func_peer_run_2']['RMSD (Mean)'])
-                dv1 = float(qap[qap['Participant'] == set][qap['Series'] == 'func_peer_run_1']['Std. DVARS (Mean)'])
-                dv2 = float(qap[qap['Participant'] == set][qap['Series'] == 'func_peer_run_2']['Std. DVARS (Mean)'])
+                fd1 = float(qap[qap['Participant'] == name][qap['Series'] == 'func_peer_run_1']['RMSD (Mean)'])
+                fd2 = float(qap[qap['Participant'] == name][qap['Series'] == 'func_peer_run_2']['RMSD (Mean)'])
+                dv1 = float(qap[qap['Participant'] == name][qap['Series'] == 'func_peer_run_1']['Std. DVARS (Mean)'])
+                dv2 = float(qap[qap['Participant'] == name][qap['Series'] == 'func_peer_run_2']['Std. DVARS (Mean)'])
 
                 fd_score = np.average([[fd1, fd2]])
                 dvars_score = np.average([dv1, dv2])
-                params.loc[set, 'mean_fd'] = fd_score
-                params.loc[set, 'dvars'] = dvars_score
+                params.loc[name, 'mean_fd'] = fd_score
+                params.loc[name, 'dvars'] = dvars_score
 
             except:
 
@@ -369,60 +405,55 @@ for set in ['sub-5773707']:
 # #############################################################################
 # Visualization for one participant in each direction
 
-x_targets = np.repeat(np.array(fixations['pos_x']), 5)*monitor_width/2
-y_targets = np.repeat(np.array(fixations['pos_y']), 5)*monitor_height/2
+# x_targets = np.repeat(np.array(fixations['pos_x']), 5)*monitor_width/2
+# y_targets = np.repeat(np.array(fixations['pos_y']), 5)*monitor_height/2
+#
+# time_series = range(0, len(x_targets))
+#
+# plt.figure()
+# plt.subplot(2, 1, 1)
+# plt.ylabel('Horizontal position')
+# plt.plot(time_series, x_targets, '.-', color='k')
+# plt.plot(time_series, predicted_x, '.-', color='b')
+# plt.subplot(2, 1, 2)
+# plt.ylabel('Vertical position')
+# plt.xlabel('TR')
+# plt.plot(time_series, y_targets, '.-', color='k')
+# plt.plot(time_series, predicted_y, '.-', color='b')
+# # plt.savefig(os.path.join(output_path, 'y_dir.png'), bbox_inches='tight', dpi=600)
+# # plt.show()
+#
+# # #############################################################################
+# # Histogram for RMSE distribution after outlier removal
 
-time_series = range(0, 135)
-
-plt.figure()
-plt.subplot(2, 1, 1)
-plt.ylabel('Horizontal position')
-plt.plot(time_series, x_targets, '.-', color='k')
-plt.plot(time_series, predicted_x, '.-', color='b')
-plt.subplot(2, 1, 2)
-plt.ylabel('Vertical position')
-plt.xlabel('TR')
-plt.plot(time_series, y_targets, '.-', color='k')
-plt.plot(time_series, predicted_y, '.-', color='b')
-# plt.savefig(os.path.join(output_path, 'y_dir.png'), bbox_inches='tight', dpi=600)
-plt.show()
-
-# #############################################################################
-# Histogram for RMSE distribution after outlier removal
-
-
-def remove_out(group):
-
-    temp = params[group][:350].tolist()
-    temp = [float(x) for x in temp]
-    temp_mean = np.array(temp).mean()
-    temp_stdv = np.array(temp).std()
-    cap = temp_mean + 3*temp_stdv
-
-    modified_group = [x for x in temp if 0 < x < cap]
-
-    return modified_group
-
-x_no_gsr = remove_out('x_error')
-y_no_gsr = remove_out('y_error')
-x_gsr = remove_out('x_error_gsr')
-y_gsr = remove_out('y_error_gsr')
-
-hist_x, bins_x = np.histogram(x_no_gsr)
-hist_y, bins_y = np.histogram(y_no_gsr)
-hist_x_gsr, bins_x_gsr = np.histogram(x_gsr)
-hist_y_gsr, bins_y_gsr = np.histogram(y_gsr)
-
-plt.figure()
-plt.title('Normalized histogram of RMSE')
-# plt.bar(bins_x[:-1], hist_x.astype(np.float32) / hist_x.sum(), width=(bins_x[1]-bins_x[0]), color='b', alpha=.5, label='No GSR')
-# plt.bar(bins_x_gsr[:-1], hist_x_gsr.astype(np.float32) / hist_x_gsr.sum(), width=(bins_x_gsr[1]-bins_x_gsr[0]), color='r', alpha=.5, label='GSR')
-plt.ylabel('Proportion')
-plt.xlabel('RMSE (y-direction)')
-plt.bar(bins_y[:-1], hist_y.astype(np.float32) / hist_y.sum(), width=(bins_y[1]-bins_y[0]), color='b', alpha=.5, label='No GSR')
-plt.bar(bins_y_gsr[:-1], hist_y_gsr.astype(np.float32) / hist_y_gsr.sum(), width=(bins_y_gsr[1]-bins_y_gsr[0]), color='r', alpha=.5, label='GSR')
-plt.legend()
-plt.savefig(os.path.join(output_path, 'y_dir_histogram'), bbox_inches='tight', dpi=600)
-plt.show()
+# params = pd.read_csv('subj_params.csv', index_col='subject', dtype=object)
+#
+# x_no_gsr = remove_out('x_error')
+# y_no_gsr = remove_out('y_error')
+# x_gsr = remove_out('x_error_gsr')
+# y_gsr = remove_out('y_error_gsr')
+#
+# xbins = np.histogram(np.hstack((x_no_gsr, x_gsr)), bins=25)[1]
+# ybins = np.histogram(np.hstack((y_no_gsr, y_gsr)), bins=25)[1]
+#
+# plt.figure()
+# plt.hist(x_no_gsr, xbins, color='r', alpha=.5, label='No GSR')
+# plt.hist(x_gsr, xbins, color='b', alpha=.5, label='GSR')
+# plt.title('RMSE distribution in the x-direction')
+# plt.ylabel('Number of participants')
+# plt.xlabel('RMSE (x-direction)')
+# plt.legend()
+# # plt.savefig(os.path.join(output_path, 'x_dir_histogram'), bbox_inches='tight', dpi=600)
+# # plt.show()
+#
+# plt.figure()
+# plt.hist(y_no_gsr, ybins, color='r', alpha=.5, label='No GSR')
+# plt.hist(y_gsr, ybins, color='b', alpha=.5, label='GSR')
+# plt.legend()
+# plt.title('RMSE distribution in the y-direction')
+# plt.ylabel('Number of participants')
+# plt.xlabel('RMSE (y-direction)')
+# # plt.savefig(os.path.join(output_path, 'y_dir_histogram'), bbox_inches='tight', dpi=600)
+# # plt.show()
 
 
