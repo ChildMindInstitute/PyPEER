0bba256b7bf3d3d7bd7fa69a500e26506c21b685
Jake Son <jake.son@childmind.org>
2018-02-12 16:52:41 -0500

    Bash script updated to copy segmentation and anatomical images with/without BET.

--- registration.sh
+++ registration.sh
@@ -1,10 +1,14 @@
 data='/data2/HBNcore/CMI_HBN_Data/MRI/RU/data/'
-ants='/data2/HBNcore/CMI_HBN_Data/MRI/RU/CPAC/output/pipeline_RU_CPAC/'
+ants_out='/data2/HBNcore/CMI_HBN_Data/MRI/RU/CPAC/output/pipeline_RU_CPAC/'
+ants_work='/data2/HBNcore/CMI_HBN_Data/MRI/RU/CPAC/working/'
 outpath='/data2/Projects/Jake/Registration_PEER'
 
-cd $data
+cd $outpath
 
-rm $outpath/command.txt
+rm copy_raw.txt
+rm copy_ants.txt
+
+cd $data
 
 ##########################################################################
 # Copy raw peer data
@@ -16,20 +20,18 @@ for sub in 'sub-5002891';do
 
 	if [ $n -gt "1" ]; then
 
-	num=0
-
 	for nifti_file in $(ls $data'/'$sub'/'func/*.nii.gz);do
 
 		if [[ $nifti_file == *"peer_run-1"* ]];then
-			echo "cp $nifti_file $outpath/$sub/raw_peer_1.nii.gz" >> $outpath/command.txt
+			echo "cp $nifti_file $outpath/$sub/raw_peer_1.nii.gz" >> $outpath/copy_raw.txt
 
 
 		elif [[ $nifti_file == *"peer_run-2"* ]];then
-			echo "cp $nifti_file $outpath/$sub/raw_peer_2.nii.gz" >> $outpath/command.txt
+			echo "cp $nifti_file $outpath/$sub/raw_peer_2.nii.gz" >> $outpath/copy_raw.txt
 
 
 		elif [[ $nifti_file == *"peer_run-3"* ]];then
-			echo "cp $nifti_file $outpath/$sub/raw_peer_3.nii.gz" >> $outpath/command.txt
+			echo "cp $nifti_file $outpath/$sub/raw_peer_3.nii.gz" >> $outpath/copy_raw.txt
 
 		fi
 
@@ -41,18 +43,37 @@ for sub in 'sub-5002891';do
 
 done
 
-cat $outpath/command.txt | parallel -j 5
+cat $outpath/copy_raw.txt | parallel -j 5
 
 ##########################################################################
 # Copy C-PAC outputs for ANTs registration
 
+cd $ants_out
 
+#for sub in $(ls);do 
+for sub in 'sub-5002891_ses-1';do
 
+	deskull=$ants_out/$sub/anatomical_brain/*
+	skull=$ants_out/$sub/anatomical_reorient/*
 
+	echo "cp $deskull $outpath/no_skull.nii.gz" >> $outpath/copy_out.txt
+	echo "cp $skull $outpath/skull.nii.gz" >> $outpath/copy_out.txt
 
+done
 
+cat $outpath/copy_out.txt | parallel -j 5
 
+cd $ants_work
 
+for sub in 'resting_preproc_sub-5002891_ses-1';do
+
+	segments=$ants_work/$sub/seg_preproc_0/WM/WM_mask/segment_seg_2_maths.nii.gz
+
+	echo "cp $segments $outpath/segments.nii.gz" >> $outpath/copy_working.txt
+
+done
+
+cat $outpath/copy_working.txt | parallel -j 5
 
 
 
