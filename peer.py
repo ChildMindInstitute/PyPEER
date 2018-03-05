7f5e2addddf9107e5c4705e537ce5edda9330313
Jake Son <jake.son@childmind.org>
2018-03-05 14:13:15 -0500

    Incorporated various elements for generalizable classifier.

--- peer.py
+++ peer.py
@@ -224,6 +224,8 @@ def axis_plot(fixations, predicted_x, predicted_y, subj, train_sets=1):
     # plt.savefig(os.path.join(output_path, 'peer_3.png'), bbox_inches='tight', dpi=600)
     plt.show()
 
+    return x_targets, y_targets
+
 # data_path = '/data2/Projects/Jake/Registration_Test/'
 data_path = '/data2/Projects/Jake/CBIC/'
 qap_path_RU = '/data2/HBNcore/CMI_HBN_Data/MRI/RU/QAP/qap_functional_temporal.csv'
@@ -616,7 +618,7 @@ def compute_icc(x, y):
 
     return ICC
 
-compute_icc(p3x, ptx)
+compute_icc(predicted_x, x_targets)
 
 from scipy.stats import pearsonr
 
@@ -632,8 +634,9 @@ print([p1_ptx, p1_p3x, p1_pty, p1_p3y])
 # #############################################################################
 # Test registered data
 
-eye_mask = nib.load('/usr/share/fsl/5.0/data/standard/MNI152_T1_2mm_eye_mask.nii.gz')
-eye_mask = nib.load('/home/json/Desktop/peer/coef_map_threshold_5.nii.gz')
+# eye_mask = nib.load('/usr/share/fsl/5.0/data/standard/MNI152_T1_2mm_eye_mask.nii.gz')
+# eye_mask = nib.load('/home/json/Desktop/peer/coef_map_threshold_05.nii.gz')
+eye_mask = nib.load('/data2/Projects/Jake/Resampled/eye_all_sub.nii.gz')
 eye_mask = eye_mask.get_data()
 
 params = pd.read_csv('subj_params.csv', index_col='subject', dtype=object)
@@ -664,6 +667,12 @@ def regi_peer(reg_list):
         scan2 = scan2.get_data()
         scan3 = nib.load(resample_path + sub + '/peer3_eyes.nii.gz')
         scan3 = scan3.get_data()
+        # scan1 = nib.load('/home/json/Desktop/test.nii.gz')
+        # scan1 = scan1.get_data()
+        # scan2 = nib.load('/home/json/Desktop/test2.nii.gz')
+        # scan2 = scan2.get_data()
+        # scan3 = nib.load('/home/json/Desktop/test3.nii.gz')
+        # scan3 = scan3.get_data()
 
         for item in [scan1, scan2, scan3]:
 
@@ -725,7 +734,9 @@ def regi_peer(reg_list):
 
         predicted_x, predicted_y = predict_fixations(x_model, y_model, test_vectors)
 
-        axis_plot(fixations, predicted_x, predicted_y, sub, train_sets=1)
+        x_targets, y_targets = axis_plot(fixations, predicted_x, predicted_y, sub, train_sets=1)
+
+        print(compute_icc(predicted_x, x_targets))
 
         x_res = []
         y_res = []
@@ -820,7 +831,7 @@ reg_list = reg_list[:50]
 #             if str(params.loc[subject, 'x_error_reg']) != 'nan':
 #                 reg_list.append(subject)
 
-eye_mask = nib.load('/data2/Projects/Jake/Resampled/eye_all_sub.nii.gz')
+eye_mask = nib.load('/home/json/Desktop/peer/coef_map_threshold_90.nii.gz')
 eye_mask = eye_mask.get_data()
 
 reg_list = ['sub-5986705','sub-5375858','sub-5292617','sub-5397290','sub-5844932','sub-5787700','sub-5797959',
@@ -831,7 +842,6 @@ reg_list = ['sub-5986705','sub-5375858','sub-5292617','sub-5397290','sub-5844932
 train_set_count = len(reg_list) - 1
 
 resample_path = '/data2/Projects/Jake/Resampled/'
-
 
 def general_classifier(reg_list):
 
@@ -844,12 +854,6 @@ def general_classifier(reg_list):
     for sub in reg_list[:train_set_count]:
 
         print('starting participant ' + str(sub))
-        # scan1 = nib.load(cpac_path + sub + '_ses-1/motion_correct_to_standard/_scan_peer_run-1/' + sub + '_task-peer_run-1_bold_calc_tshift_resample_volreg_antswarp.nii.gz')
-        # scan1 = scan1.get_data()
-        # scan2 = nib.load(cpac_path + sub + '_ses-1/motion_correct_to_standard/_scan_peer_run-2/' + sub + '_task-peer_run-2_bold_calc_tshift_resample_volreg_antswarp.nii.gz')
-        # scan2 = scan2.get_data()
-        # scan3 = nib.load(cpac_path + sub + '_ses-1/motion_correct_to_standard/_scan_peer_run-3/' + sub + '_task-peer_run-3_bold_calc_tshift_resample_volreg_antswarp.nii.gz')
-        # scan3 = scan3.get_data()
 
         scan1 = nib.load(resample_path + sub + '/peer1_eyes.nii.gz')
         scan1 = scan1.get_data()
@@ -868,8 +872,11 @@ def general_classifier(reg_list):
 
         for item in [scan1, scan2, scan3]:
 
+            print('Initial average: ' + str(np.average(item)))
             item = mean_center_var_norm(item)
+            print('Mean centered average: ' + str(np.average(item)))
             item = gs_regress(item, 0, item.shape[0]-1, 0, item.shape[1]-1, 0, item.shape[2]-1)
+            print('GSR average: ' + str(np.average(item)))
 
         listed1 = []
         listed2 = []
@@ -933,11 +940,11 @@ def general_classifier(reg_list):
 
     for gen in range(len(reg_list)):
 
-    # gen = 1
         gen = gen+1
         predicted_x = x_model.predict(full_test[scan1.shape[3]*(gen-1):scan1.shape[3]*(gen)])
         predicted_y = y_model.predict(full_test[scan1.shape[3]*(gen-1):scan1.shape[3]*(gen)])
         axis_plot(fixations, predicted_x, predicted_y, sub, train_sets=1)
+
 
 
         x_res = []
@@ -973,23 +980,148 @@ time_series = range(0, len(p1))
 plt.figure(figsize=(15, 10))
 plt.subplot(2, 1, 1)
 plt.ylabel('Horizontal position')
+plt.title('Eye mask comparison - sub-5343770')
 plt.plot(time_series, x_targets, '.-', color='k', label='Fixations')
-plt.plot(time_series, p1, '.-', color='b', label='General')
-plt.plot(time_series, p3, '.-', color='r', label='Subject')
+plt.plot(time_series, p1, '.-', color='b', label='90th%')
+plt.plot(time_series, p3, '.-', color='r', label='50th%')
+plt.plot(time_series, p5, '.-', color='g', label='25th%')
+plt.plot(time_series, p7, '.-', color='m', label='10th%')
+plt.plot(time_series, p9, '.-', color='y', label='05th%')
 plt.legend()
 plt.subplot(2, 1, 2)
 plt.ylabel('Vertical position')
 plt.xlabel('TR')
 plt.plot(time_series, y_targets, '.-', color='k', label='Fixations')
-plt.plot(time_series, p2, '.-', color='b', label='General')
-plt.plot(time_series, p4, '.-', color='r', label='Subject')
+plt.plot(time_series, p2, '.-', color='b', label='90th%')
+plt.plot(time_series, p4, '.-', color='r', label='50th%')
+plt.plot(time_series, p6, '.-', color='g', label='25th%')
+plt.plot(time_series, p8, '.-', color='m', label='10th%')
+plt.plot(time_series, p10, '.-', color='y', label='05th%')
 plt.legend()
 plt.show()
 
 # #############################################################################
+# Turning each set of weights into a Nifti image for coefficient of variation map analysis
+
+reg_list = ['sub-5986705','sub-5375858','sub-5292617','sub-5397290','sub-5844932','sub-5787700','sub-5797959',
+            'sub-5378545','sub-5085726','sub-5984037','sub-5076391','sub-5263388','sub-5171285',
+            'sub-5917648','sub-5814325','sub-5169146','sub-5484500','sub-5481682','sub-5232535','sub-5905922',
+            'sub-5975698','sub-5986705','sub-5343770']
+
+train_set_count = len(reg_list) - 1
+
+resample_path = '/data2/Projects/Jake/Resampled/'
+
+eye_mask = nib.load('/data2/Projects/Jake/Resampled/eye_all_sub.nii.gz')
+eye_mask = eye_mask.get_data()
+
+for sub in reg_list:
+
+    train_vectors1 = []
+    train_vectors2 = []
+    test_vectors = []
+
+    print('starting participant ' + str(sub))
+
+    scan1 = nib.load(resample_path + sub + '/peer1_eyes.nii.gz')
+    scan1 = scan1.get_data()
+    scan2 = nib.load(resample_path + sub + '/peer2_eyes.nii.gz')
+    scan2 = scan2.get_data()
+    scan3 = nib.load(resample_path + sub + '/peer3_eyes.nii.gz')
+    scan3 = scan3.get_data()
+
+    for item in [scan1, scan2, scan3]:
+
+        for vol in range(item.shape[3]):
+            output = np.multiply(eye_mask, item[:, :, :, vol])
+
+            item[:, :, :, vol] = output
+
+    for item in [scan1, scan2, scan3]:
+        print('Initial average: ' + str(np.average(item)))
+        item = mean_center_var_norm(item)
+        print('Mean centered average: ' + str(np.average(item)))
+        item = gs_regress(item, 0, item.shape[0] - 1, 0, item.shape[1] - 1, 0, item.shape[2] - 1)
+        print('GSR average: ' + str(np.average(item)))
+
+    listed1 = []
+    listed2 = []
+    listed_testing = []
+
+    print('beginning vectors')
+
+    for tr in range(int(scan1.shape[3])):
+        tr_data1 = scan1[:, :, :, tr]
+        vectorized1 = np.array(tr_data1.ravel())
+        listed1.append(vectorized1)
+
+        tr_data2 = scan3[:, :, :, tr]
+        vectorized2 = np.array(tr_data2.ravel())
+        listed2.append(vectorized2)
+
+        te_data = scan2[:, :, :, tr]
+        vectorized_testing = np.array(te_data.ravel())
+        listed_testing.append(vectorized_testing)
+
+    train_vectors1.append(listed1)
+    test_vectors.append(listed_testing)
+    train_vectors2.append(listed2)
+
+    full_train1 = []
+    full_test = []
+    full_train2 = []
+
+    for part in range(len(reg_list[:train_set_count])):
+        for vect in range(scan1.shape[3]):
+            full_train1.append(train_vectors1[part][vect])
+            full_test.append(test_vectors[part][vect])
+            full_train2.append(train_vectors2[part][vect])
+
+        # train_vectors1 = np.asarray(listed1)
+        # test_vectors = np.asarray(listed_testing)
+        # train_vectors2 = np.asarray(listed2)
+
+        # #############################################################################
+        # Averaging training signal
+
+    print('average vectors')
+
+    train_vectors = data_processing(3, full_train1, full_train2)
+
+    # #############################################################################
+    # Import coordinates for fixations
+
+    print('importing fixations')
+
+    fixations = pd.read_csv('stim_vals.csv')
+    x_targets = np.tile(np.repeat(np.array(fixations['pos_x']), len(reg_list[:train_set_count])) * monitor_width / 2, 3 - 1)
+    y_targets = np.tile(np.repeat(np.array(fixations['pos_y']), len(reg_list[:train_set_count])) * monitor_height / 2,
+                        3 - 1)
+
+    # #############################################################################
+    # Create SVR Model
+
+    x_model, y_model = create_model(train_vectors, x_targets, y_targets)
+
+    x_model_coef = x_model.coef_
+    y_model_coef = y_model.coef_
+
+    x_model_coef = np.array(x_model_coef).reshape((35, 17, 14))
+    y_model_coef = np.array(y_model_coef).reshape((35, 17, 14))
+
+    img = nib.Nifti1Image(x_model_coef, np.eye(4))
+    img.header['pixdim'] = np.array([-1, 3, 3, 3, .80500031, 0, 0, 0])
+    img.to_filename('/data2/Projects/Jake/weights_coef/' + str(sub) + 'x.nii.gz')
+    img = nib.Nifti1Image(y_model_coef, np.eye(4))
+    img.header['pixdim'] = np.array([-1, 3, 3, 3, .80500031, 0, 0, 0])
+    img.to_filename('/data2/Projects/Jake/weights_coef/' + str(sub) + 'y.nii.gz')
+
+
+
+# #############################################################################
 # Creating a coefficient of variation map
 
-total = nib.load('/data2/Projects/Jake/coef_var/total.nii.gz')
+total = nib.load('/data2/Projects/Jake/weights_coef/totalx.nii.gz')
 data = total.get_data()
 
 coef_array = np.zeros((data.shape[0], data.shape[1], data.shape[2]))
@@ -1002,51 +1134,145 @@ for x in range(data.shape[0]):
             vstdev = np.std(np.array(data[x, y, z, :]))
 
             for time in range(data.shape[3]):
-                coef_array[x, y, z] = float(vstdev/vmean)
+                if np.round(vmean, 2) == 0.00:
+                    coef_array[x, y, z] = float(vstdev)
+                else:
+                    coef_array[x, y, z] = float(vstdev)
 
 img = nib.Nifti1Image(coef_array, np.eye(4))
 img.header['pixdim'] = np.array([-1, 3, 3, 3, .80500031, 0, 0, 0])
-img.to_filename('/home/json/Desktop/peer/coef_map.nii.gz')
+img.to_filename('/data2/Projects/Jake/weights_coef/x_coef_map_stdev.nii.gz')
 
-modified = nib.load('/home/json/Desktop/peer/coef_map.nii.gz')
+modified = nib.load('/data2/Projects/Jake/weights_coef/x_coef_map.nii.gz')
 data = modified.get_data()
 
 for x in range(data.shape[0]):
     for y in range(data.shape[1]):
         for z in range(data.shape[2]):
-            if data[x, y, z] < .5:
+            if abs(data[x, y, z]) > 100 or abs(data[x, y, z] < 3) and abs(np.round(data[x, y, z],2) != 0.00):
                 data[x, y, z] = 1
             else:
                 data[x, y, z] = 0
 
 img = nib.Nifti1Image(data, np.eye(4))
 img.header['pixdim'] = np.array([-1, 3, 3, 3, .80500031, 0, 0, 0])
-img.to_filename('/home/json/Desktop/peer/coef_map_threshold_5.nii.gz')
+img.to_filename('/data2/Projects/Jake/eye_masks/x_coef_map_eyes_100_5.nii.gz')
 
 # #############################################################################
 # Get distribution of voxel intensities from isolated eye coefficient of variation map to determine intensity threshold
 
-coef_sub = nib.load('/home/json/Desktop/peer/coef_map_sub.nii.gz')
+coef_sub = nib.load('/data2/Projects/Jake/weights_coef/x_coef_map.nii.gz')
 data = coef_sub.get_data()
 
 data_rav = data.ravel()
+data_rav = np.nan_to_num(data_rav)
+data_rav = np.array([x for x in data_rav if x != 0])
 
-xbins = np.histogram(data_rav, bins=30)[1]
+xbins = np.histogram(data_rav, bins=300)[1]
 
 values, base = np.histogram(data_rav, bins=30)
 cumulative = np.cumsum(values)
 
 plt.figure()
-plt.hist(data_rav/len(data_rav), xbins, color='b')
+plt.hist(data_rav, xbins, color='b')
 plt.title('Full Raw')
 # plt.savefig('/home/json/Desktop/peer/eye_distr.png')
 plt.show()
 # plt.plot(base[:-1], cumulative/len(data_rav), color='g')
 # plt.show()
 
+# #############################################################################
 # Determine percentiles
-values, base = np.histogram(data_rav, bins=100)
+
+values, base = np.histogram(data_rav, bins=len(data_rav))
+cumulative = np.cumsum(values)/len(data_rav)
+
+for num in range(len(data_rav)):
+    if np.round(cumulative[num], 3) == .05:
+        print(base[num])
+
 # value_of_interest = base[percentile]
+
+# #############################################################################
+# Compare eye masks for a given subject
+
+for item in os.listdir('/data2/Projects/Jake/eye_masks'):
+for item in ['x_coef_map_eyes_1.nii.gz', 'x_coef_map_eyes_5.nii.gz', 'x_coef_map_eyes_10.nii.gz']:
+
+    eye_mask = nib.load('/data2/Projects/Jake/eye_masks/' + str(item)).get_data()
+
+    for sub in reg_list:
+
+        print('starting participant ' + str(sub))
+        scan1 = nib.load(resample_path + sub + '/peer1_eyes.nii.gz')
+        scan1 = scan1.get_data()
+        scan2 = nib.load(resample_path + sub + '/peer2_eyes.nii.gz')
+        scan2 = scan2.get_data()
+        scan3 = nib.load(resample_path + sub + '/peer3_eyes.nii.gz')
+        scan3 = scan3.get_data()
+
+        for item in [scan1, scan2, scan3]:
+
+            for vol in range(item.shape[3]):
+
+                output = np.multiply(eye_mask, item[:, :, :, vol])
+
+                item[:, :, :, vol] = output
+
+        for item in [scan1, scan2, scan3]:
+
+            item = mean_center_var_norm(item)
+            item = gs_regress(item, 0, item.shape[0] - 1, 0, item.shape[1] - 1, 0, item.shape[2] - 1)
+
+        listed1 = []
+        listed2 = []
+        listed_testing = []
+
+        print('beginning vectors')
+
+        for tr in range(int(scan1.shape[3])):
+
+            tr_data1 = scan1[:, :, :, tr]
+            vectorized1 = np.array(tr_data1.ravel())
+            listed1.append(vectorized1)
+
+            tr_data2 = scan3[:, :, :, tr]
+            vectorized2 = np.array(tr_data2.ravel())
+            listed2.append(vectorized2)
+
+            te_data = scan2[:, :, :, tr]
+            vectorized_testing = np.array(te_data.ravel())
+            listed_testing.append(vectorized_testing)
+
+        train_vectors1 = np.asarray(listed1)
+        test_vectors = np.asarray(listed_testing)
+        train_vectors2 = np.asarray(listed2)
+
+        # #############################################################################
+        # Averaging training signal
+
+        print('average vectors')
+
+        train_vectors = data_processing(3, train_vectors1, train_vectors2)
+
+        # #############################################################################
+        # Import coordinates for fixations
+
+        print('importing fixations')
+
+        fixations = pd.read_csv('stim_vals.csv')
+        x_targets = np.tile(np.repeat(np.array(fixations['pos_x']), 1) * monitor_width / 2, 3 - 1)
+        y_targets = np.tile(np.repeat(np.array(fixations['pos_y']), 1) * monitor_height / 2, 3 - 1)
+
+        # #############################################################################
+        # Create SVR Model
+
+        x_model, y_model = create_model(train_vectors, x_targets, y_targets)
+
+        predicted_x, predicted_y = predict_fixations(x_model, y_model, test_vectors)
+
+        axis_plot(fixations, predicted_x, predicted_y, sub, train_sets=1)
+
 
 # #############################################################################
 # Visualize error vs motion
