ff9df2dc33389ce5fee1d46482024eec574cd8f6
Jake Son <jake.son@childmind.org>
2018-05-19 09:51:02 -0400

    Fixed error in ICC calculation

--- modality_icc.R
+++ modality_icc.R
@@ -9,8 +9,10 @@ x_et_group = et_median$x
 y_peer_group = peer_median$y
 y_et_group = et_median$y
 
-subjects = c()
-correlations = c()
+x_subjects = c()
+x_correlations = c()
+y_subjects = c()
+y_correlations = c()
 
 for (sub in sub_list) {
   
@@ -26,15 +28,21 @@ for (sub in sub_list) {
     
     if (length(x_et) == 250 & length(x_peer) == 250) {
       
-      subjects = rbind(subjects, sub, sub)
-      correlations = rbind(correlations, cor(x_et, x_et_group, method='pearson'), cor(y_et, y_et_group, method='pearson'))
-      
+      x_subjects = rbind(subjects, sub, sub)
+      y_subjects = rbind(subjects, sub, sub)
+      x_correlations = rbind(correlations, cor(x_et, x_et_group, method='pearson'), cor(x_peer, x_peer_group, method='pearson'))
+      y_correlations = rbind(correlations, cor(y_et, y_et_group, method='pearson'), cor(y_peer, y_peer_group, method='pearson'))
     }
     
   }
   
 }
 
-df = data.frame(subjects, correlations)
+x_df = data.frame(x_subjects, x_correlations)
+y_df = data.frame(y_subjects, y_correlations)
 
-icc_val = ICCbare(c('subjects'), c('correlations'), df)
+x_icc = ICCbare(c('x_subjects'), c('x_correlations'), x_df)
+y_icc = ICCbare(c('y_subjects'), c('y_correlations'), y_df)
+
+
+
